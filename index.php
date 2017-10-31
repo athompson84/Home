e2e14179836dd9e959c14a572baa1408d90464f8
Rico <anththompson84@gmail.com>
2017-10-30 20:57:43 -0400

    First commit for my homepage

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Anthony Thompson</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+  </head>
+  <body >
+   
+     <div class="nav"> 
+          <li><a href="">HOME</a></li>
+          <li><a href="">ABOUT ME</a></li>
+          <li><a href="">PORTFOLIO</a></li>
+          <li><a href="">CONTACT</a></li>
+    </div>
+         <h1>Anthony Thompson</h1>
+        
+
+     
+  </body>
+</html> 
